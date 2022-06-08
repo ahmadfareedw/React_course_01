cc0c657281b308d11d87dbe8994bfeec8b892d6a
ahmadfareed <a.fareed@pioneerlogics.com>
2022-06-08 18:22:35 +0500

    food order app in development

--- 04-FoodOrderApp/src/components/Layout/Header.js
+++ 04-FoodOrderApp/src/components/Layout/Header.js
@@ -1,15 +1,17 @@
 import { Fragment } from "react";
-import mealsImae from "../../assets/meals.jpg";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
-      <header>
+      <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCardButton />
       </header>
-      <div>
-        <img />
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
