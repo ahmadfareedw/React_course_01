e4c69d4c0023edcb0277323b639bba0b857dd05a
ahmadfareed <a.fareed@pioneerlogics.com>
2022-06-13 18:18:18 +0500

    context added along with reducers

--- 04-FoodOrderApp/src/components/Cart/Cart.js
+++ 04-FoodOrderApp/src/components/Cart/Cart.js
@@ -1,26 +1,35 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCTX = useContext(CartContext);
+
+  const totalAmount = `$${cartCTX.totalAmount.toFixed(2)}`;
+  const hasItems = cartCTX.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {" "}
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+      {cartCTX.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
