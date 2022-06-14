f2ebf25424dac555b7f2cb41f9f470c579313897
ahmadfareed <a.fareed@pioneerlogics.com>
2022-06-14 12:14:36 +0500

    FoodOrderApp Completed

--- 04-FoodOrderApp/src/components/Cart/Cart.js
+++ 04-FoodOrderApp/src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCTX = useContext(CartContext);
@@ -9,11 +10,26 @@ const Cart = (props) => {
   const totalAmount = `$${cartCTX.totalAmount.toFixed(2)}`;
   const hasItems = cartCTX.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {
+    cartCTX.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCTX.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {" "}
       {cartCTX.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
--- 04-FoodOrderApp/src/store/CartProvider.js
+++ 04-FoodOrderApp/src/store/CartProvider.js
@@ -7,16 +7,77 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+  // ADD Logic
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
+    // finding out that if item exits in the array
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    // puuling out item , incase of null variable will have null as false value
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    // checking if item is found
+    if (existingCartItem) {
+      // creating the updated item to be added in the array later
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      // creating new array and adding old items from state
+      updatedItems = [...state.items];
+      // overriding the exisiting item with new updated values
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      // if no item is found then add as a new item to cart by concat method of JS
+      updatedItems = state.items.concat(action.item);
+    }
+
+    // returning new state
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+  // ********************** ADD Ends **************************************
+
+  // REMOVE Logic
+  if (action.type === "REMOVE") {
+    // checking if the item already exixts and getting its index
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    // pulling out the found item using index
+    const existingItem = state.items[existingCartItemIndex];
+    // updating found item's price by subtracting 1 item's price from it
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    // checking if only one item exixts if so then remove whole item from array
+    if (existingItem.amount === 1) {
+      // creating a new updated array excluding that one item
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      // incase more than 1 items exist then only update it's value by -1
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      // copying old array to new one
+      updatedItems = [...state.items];
+      // overriding that one item using index found earlier
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    // returning new state
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
