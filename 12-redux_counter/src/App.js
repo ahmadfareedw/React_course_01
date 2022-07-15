e06da2e756f51cae940630b4c7818ce37ee5e993
ahmadfareedw <ahmadfareed.work@gmail.com>
2022-07-15 17:24:02 +0500

    completed redux basics

--- 12-redux_counter/src/App.js
+++ 12-redux_counter/src/App.js
@@ -1,9 +1,18 @@
-import Counter from './components/Counter';
-
+import React from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <React.Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      <Counter />
+    </React.Fragment>
   );
 }
 
--- 12-redux_counter/src/store/index.js
+++ 12-redux_counter/src/store/index.js
@@ -1,26 +1,6 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++; // still imutable code for state
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.amount;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === "increment") {
@@ -55,7 +35,7 @@ const counterSlice = createSlice({
 // };
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
 export default store;
